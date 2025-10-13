6146f050f5bcffe82705c99c9c7903aa5b4b68eb
Elijah Jacob <elijahgjacob@berkeley.edu>
2025-10-12 22:15:10 -0400

    Temporarily disable Contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
-import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Resume from '../components/Sections/Resume';
@@ -20,7 +19,6 @@ const Home: FC = memo(() => {
       <Hero />
       <About />
       <Resume />
-      <Contact />
       <Footer />
     </Page>
   );
