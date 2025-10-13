ec72568a3f34bdd333189184d388414358ebefc8
Elijah Jacob <elijahgjacob@berkeley.edu>
2025-10-12 21:57:57 -0400

    Make company locations clickable links

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -4,7 +4,7 @@ import {FC, memo} from 'react';
 import {TimelineItem} from '../../../data/dataDef';
 
 const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
-  const {title, date, location, content, logo} = item;
+  const {title, date, location, content, logo, locationUrl} = item;
   return (
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
       <div className="flex flex-col pb-4">
@@ -17,7 +17,17 @@ const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
           <h2 className="text-xl font-bold">{title}</h2>
         </div>
         <div className="flex items-center justify-center gap-x-2 md:justify-start">
-          <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
+          {locationUrl ? (
+            <a
+              className="flex-1 text-sm font-medium italic hover:text-orange-600 hover:underline sm:flex-none"
+              href={locationUrl}
+              rel="noopener noreferrer"
+              target="_blank">
+              {location}
+            </a>
+          ) : (
+            <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
+          )}
           <span>•</span>
           <span className="flex-1 text-sm sm:flex-none">{date}</span>
         </div>
