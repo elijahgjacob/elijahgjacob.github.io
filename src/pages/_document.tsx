cd218b1a73b86b41e6269fe8a48e881f2f840351
Elijah Jacob <elijahgjacob@berkeley.edu>
2025-10-13 23:54:50 -0400

    Upgrade to more distinctive tech fonts: Orbitron for headings, Rajdhani for body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Inter:wght@300;400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&family=Rajdhani:wght@400;500;600;700&family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
